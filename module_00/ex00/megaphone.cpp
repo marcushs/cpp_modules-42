7b78e47f7c603e189154bf3dbd00599e1008813c
marcushs <marcus.hoksum.leung@gmail.com>
2023-06-20 21:18:35 +0200

    add ex00 megaphone

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < argc && argc > 1; i++)
+	{
+		for (size_t j = 0; j < strlen(argv[i]); j++)
+			argv[i][j] = toupper(argv[i][j]);
+		std::cout << argv[i];
+	}
+	std::cout << std::endl;
+}

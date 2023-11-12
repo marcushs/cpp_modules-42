984b59eb0bab0150ae57e13033aa7e53784568cd
Marcus Leung <marcus.hoksum.leung@gmail.com>
2023-11-12 19:01:57 +0100

    add cpp00/ex01

--- module_00/ex01/Contact.hpp
+++ module_00/ex01/Contact.hpp
@@ -1,26 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hleung <hleung@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/12 17:36:02 by hleung            #+#    #+#             */
-/*   Updated: 2023/11/12 17:36:34 by hleung           ###   ########.fr       */
+/*   Created: 2023/11/12 18:32:03 by hleung            #+#    #+#             */
+/*   Updated: 2023/11/12 18:32:04 by hleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-int	main(int argc, char **argv)
-{
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1; i < argc && argc > 1; i++)
-	{
-		for (size_t j = 0; argv[i][j]; j++)
-			argv[i][j] = std::toupper(argv[i][j]);
-		std::cout << argv[i];
-	}
-	std::cout << std::endl;
-}
--- module_00/ex01/PhoneBook.hpp
+++ module_00/ex01/PhoneBook.hpp
@@ -1,26 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hleung <hleung@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/12 17:36:02 by hleung            #+#    #+#             */
-/*   Updated: 2023/11/12 17:36:34 by hleung           ###   ########.fr       */
+/*   Created: 2023/11/12 18:32:08 by hleung            #+#    #+#             */
+/*   Updated: 2023/11/12 18:59:29 by hleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(int argc, char **argv)
+int main(void)
 {
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1; i < argc && argc > 1; i++)
-	{
-		for (size_t j = 0; argv[i][j]; j++)
-			argv[i][j] = std::toupper(argv[i][j]);
-		std::cout << argv[i];
-	}
-	std::cout << std::endl;
+    char buf[100];
+    
+    std::getline()
 }

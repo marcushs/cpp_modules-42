6d81d2d62943d5aeab96e88047a79dd03b1a5b28
Hok sum Leung <hleung@z1r2p5.42lyon.fr>
2023-11-15 17:20:12 +0100

    created ex02 file

---
+++ module_00/ex02/main.cpp
@@ -0,0 +1,10 @@
+
+// setw example
+#include <iostream>     // std::cout, std::endl
+#include <iomanip>      // std::setw
+
+int main () {
+  std::cout << std::setw(10);
+  std::cout << 777777777777777 << std::endl;
+  return 0;
+}

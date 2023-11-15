6b4d06b91b4f8524c064952c92453fef137fc7af
Hok sum Leung <hleung@z1r2p5.42lyon.fr>
2023-11-15 17:19:43 +0100

    mod megaphone to use string instead of argv

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hleung <hleung@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: hleung <hleung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 17:36:02 by hleung            #+#    #+#             */
-/*   Updated: 2023/11/12 17:36:34 by hleung           ###   ########.fr       */
+/*   Updated: 2023/11/15 15:11:49 by hleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int	main(int argc, char **argv)
 {
@@ -18,9 +19,10 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < argc && argc > 1; i++)
 	{
-		for (size_t j = 0; argv[i][j]; j++)
-			argv[i][j] = std::toupper(argv[i][j]);
-		std::cout << argv[i];
+		std::string str = argv[i];
+		for (size_t j = 0; str[j]; j++)
+			str[j] = std::toupper(str[j]);
+		std::cout << str;
 	}
 	std::cout << std::endl;
 }

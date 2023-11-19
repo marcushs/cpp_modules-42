c31d63c62e6c8973dcb2a7bbbaf3fac4afb098d6
Marcus Leung <marcus.hoksum.leung@gmail.com>
2023-11-19 08:18:54 +0100

    copy infile to infile.replace

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hleung <hleung@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 11:30:24 by hleung            #+#    #+#             */
-/*   Updated: 2023/11/18 20:29:53 by hleung           ###   ########.fr       */
+/*   Updated: 2023/11/19 08:17:31 by hleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,19 @@ int	main(int argc, char **argv)
 		for (int i = 0; i < 3; i++)
 			args[i] = argv[i + 1];
 		std::ifstream	ifs(args[0].c_str());
-		std::string cpy;
-		ifs >> cpy;
+		if (!ifs.is_open())
+		{
+			std::cout << "Error opening " << args[0] << std::endl;
+			return (1);
+		}
+		std::ofstream	ofs(args[0].append(".replace").c_str());
+		char c;
+		while (ifs.get(c))
+		{
+			ofs << c;
+		}
 		ifs.close();
 		// std::cout << args[0].append(".replace") << std::endl;
-		std::ofstream	ofs(args[0].append(".replace").c_str());
-		ofs << cpy;
 		ofs.close();
 		// ifs.close();
 		return (0);

6914b628479f50095843dfbb27e66f97b8d810b0
Hok sum Leung <hleung@z2r2p1.42lyon.fr>
2023-12-13 17:09:22 +0100

    Need to mod module04 for deep copy (=)

--- module_04/ex01/src/main.cpp
+++ module_04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hleung <hleung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 07:31:23 by hleung            #+#    #+#             */
-/*   Updated: 2023/12/13 10:40:29 by hleung           ###   ########.fr       */
+/*   Updated: 2023/12/13 16:55:26 by hleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,53 +19,80 @@
 
 int	main(void)
 {
-	{	
-		Animal	*animalFarm[10];
+	// {	
+	// 	Animal	*animalFarm[10];
 		
-		for (int i = 0; i < 10; i++)
-			(i % 2 == 0) ? animalFarm[i] = new Cat() : animalFarm[i] = new Dog();
+	// 	for (int i = 0; i < 10; i++)
+	// 		(i % 2 == 0) ? animalFarm[i] = new Cat() : animalFarm[i] = new Dog();
 
-		for (int i = 0; i < 10; i++)
-			animalFarm[i]->makeSound();
+	// 	for (int i = 0; i < 10; i++)
+	// 		animalFarm[i]->makeSound();
 
-		for (int i = 0; i < 10; i++)
-			delete animalFarm[i];
-	}
+	// 	for (int i = 0; i < 10; i++)
+	// 		delete animalFarm[i];
+	// }
 
-	std::cout << "---------------------------" << std::endl;
+	// std::cout << "---------------------------" << std::endl;
+	
+	// {
+	// 	Dog	*a = new Dog();
+	// 	a->makeSound();
+	// 	a->getBrain()->setIdea(10, "idea: eat bone in 3 minutes");
+	// 	std::cout << a->getBrain()->getIdea(10) << std::endl;
+	// 	Dog	*b = new Dog(*a);
+		
+	// 	std::cout << b->getBrain()->getIdea(10) << std::endl;
+	// 	b->getBrain()->setIdea(0, "idea: index 0");
+	// 	std::cout << b->getBrain()->getIdea(0) << std::endl;
+	// 	std::cout << a->getBrain()->getIdea(0) << std::endl;
+		
+	// 	delete a;
+	// 	delete b;
+	// }
+	
+	// std::cout << "---------------------------" << std::endl;
+	
+	// {
+	// 	Cat	*a = new Cat();
+	// 	a->makeSound();
+	// 	a->getBrain()->setIdea(10, "idea: eat fish NOW!");
+	// 	std::cout << a->getBrain()->getIdea(10) << std::endl;
+	// 	Cat	*b = new Cat(*a);
+		
+	// 	std::cout << b->getBrain()->getIdea(10) << std::endl;
+	// 	b->getBrain()->setIdea(0, "idea: index 0");
+	// 	std::cout << b->getBrain()->getIdea(0) << std::endl;
+	// 	std::cout << a->getBrain()->getIdea(0) << std::endl;
+		
+	// 	delete a;
+	// 	delete b;
+	// }
 	
 	{
-		Dog	*a = new Dog();
-		a->makeSound();
-		a->getBrain()->setIdea(10, "idea: eat bone in 3 minutes");
-		std::cout << a->getBrain()->getIdea(10) << std::endl;
-		Dog	*b = new Dog(*a);
+		Dog	*dog = new Dog();
+		Dog	*dog2 = new Dog();
+
+		dog->getBrain()->setIdea(10, "dog index 10");
+		dog2->getBrain()->setIdea(10, "dog2 index 10");
+
+		std::cout << dog->getBrain()->getIdea(10) << std::endl;
+		std::cout << dog2->getBrain()->getIdea(10) << std::endl;
+
+		*dog = *dog2;
+
+		std::cout << dog->getBrain()->getIdea(10) << std::endl;
+		std::cout << dog2->getBrain()->getIdea(10) << std::endl;
 		
-		std::cout << b->getBrain()->getIdea(10) << std::endl;
-		b->getBrain()->setIdea(0, "idea: index 0");
-		std::cout << b->getBrain()->getIdea(0) << std::endl;
-		std::cout << a->getBrain()->getIdea(0) << std::endl;
+		dog->getBrain()->setIdea(11, "dog index 11");
+		dog2->getBrain()->setIdea(11, "dog2 index 11");
+
+		std::cout << dog->getBrain()->getIdea(10) << std::endl;
+		std::cout << dog2->getBrain()->getIdea(10) << std::endl;
+		std::cout << dog->getBrain()->getIdea(11) << std::endl;
+		std::cout << dog2->getBrain()->getIdea(11) << std::endl;
 		
-		delete a;
-		delete b;
+		delete dog;
+		delete dog2;
 	}
-	
-	std::cout << "---------------------------" << std::endl;
-	
-	{
-		Cat	*a = new Cat();
-		a->makeSound();
-		a->getBrain()->setIdea(10, "idea: eat fish NOW!");
-		std::cout << a->getBrain()->getIdea(10) << std::endl;
-		Cat	*b = new Cat(*a);
-		
-		std::cout << b->getBrain()->getIdea(10) << std::endl;
-		b->getBrain()->setIdea(0, "idea: index 0");
-		std::cout << b->getBrain()->getIdea(0) << std::endl;
-		std::cout << a->getBrain()->getIdea(0) << std::endl;
-		
-		delete a;
-		delete b;
-	}
-	
+
 }
--- module_04/ex02/src/main.cpp
+++ module_04/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hleung <hleung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 07:31:23 by hleung            #+#    #+#             */
-/*   Updated: 2023/12/13 11:15:14 by hleung           ###   ########.fr       */
+/*   Updated: 2023/12/13 16:31:42 by hleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,52 +25,63 @@ int	main(void)
 	// 	delete a;
 	// }
 	
-	{	
-		Animal	*animalFarm[10];
+	// {	
+	// 	Animal	*animalFarm[10];
 		
-		for (int i = 0; i < 10; i++)
-			(i % 2 == 0) ? animalFarm[i] = new Cat() : animalFarm[i] = new Dog();
+	// 	for (int i = 0; i < 10; i++)
+	// 		(i % 2 == 0) ? animalFarm[i] = new Cat() : animalFarm[i] = new Dog();
 
-		for (int i = 0; i < 10; i++)
-			animalFarm[i]->makeSound();
+	// 	for (int i = 0; i < 10; i++)
+	// 		animalFarm[i]->makeSound();
 
-		for (int i = 0; i < 10; i++)
-			delete animalFarm[i];
-	}
+	// 	for (int i = 0; i < 10; i++)
+	// 		delete animalFarm[i];
+	// }
 
-	std::cout << "---------------------------" << std::endl;
+	// std::cout << "---------------------------" << std::endl;
 	
+	// {
+	// 	Dog	*a = new Dog();
+	// 	a->makeSound();
+	// 	a->getBrain()->setIdea(10, "idea: eat bone in 3 minutes");
+	// 	std::cout << a->getBrain()->getIdea(10) << std::endl;
+	// 	Dog	*b = new Dog(*a);
+		
+	// 	std::cout << b->getBrain()->getIdea(10) << std::endl;
+	// 	b->getBrain()->setIdea(0, "idea: index 0");
+	// 	std::cout << b->getBrain()->getIdea(0) << std::endl;
+	// 	std::cout << a->getBrain()->getIdea(0) << std::endl;
+		
+	// 	delete a;
+	// 	delete b;
+	// }
+	
+	// std::cout << "---------------------------" << std::endl;
+	
+	// {
+	// 	Cat	*a = new Cat();
+	// 	a->makeSound();
+	// 	a->getBrain()->setIdea(10, "idea: eat fish NOW!");
+	// 	std::cout << a->getBrain()->getIdea(10) << std::endl;
+	// 	Cat	*b = new Cat(*a);
+		
+	// 	std::cout << b->getBrain()->getIdea(10) << std::endl;
+	// 	b->getBrain()->setIdea(0, "idea: index 0");
+	// 	std::cout << b->getBrain()->getIdea(0) << std::endl;
+	// 	std::cout << a->getBrain()->getIdea(0) << std::endl;
+		
+	// 	delete a;
+	// 	delete b;
+	// }
+
 	{
-		Dog	*a = new Dog();
-		a->makeSound();
-		a->getBrain()->setIdea(10, "idea: eat bone in 3 minutes");
-		std::cout << a->getBrain()->getIdea(10) << std::endl;
-		Dog	*b = new Dog(*a);
-		
-		std::cout << b->getBrain()->getIdea(10) << std::endl;
-		b->getBrain()->setIdea(0, "idea: index 0");
-		std::cout << b->getBrain()->getIdea(0) << std::endl;
-		std::cout << a->getBrain()->getIdea(0) << std::endl;
-		
-		delete a;
-		delete b;
-	}
-	
-	std::cout << "---------------------------" << std::endl;
-	
-	{
-		Cat	*a = new Cat();
-		a->makeSound();
-		a->getBrain()->setIdea(10, "idea: eat fish NOW!");
-		std::cout << a->getBrain()->getIdea(10) << std::endl;
-		Cat	*b = new Cat(*a);
-		
-		std::cout << b->getBrain()->getIdea(10) << std::endl;
-		b->getBrain()->setIdea(0, "idea: index 0");
-		std::cout << b->getBrain()->getIdea(0) << std::endl;
-		std::cout << a->getBrain()->getIdea(0) << std::endl;
-		
-		delete a;
-		delete b;
+		Dog	*dog = new Dog();
+		Dog	*dog2 = new Dog();
+
+		dog->getBrain()->setIdea(10, "dog index 10");
+		dog2->getBrain()->setIdea(10, "dog2 index 10");
+
+		std::cout << dog->getBrain()->getIdea(10) << std::endl;
+		std::cout << dog2->getBrain()->getIdea(10) << std::endl;
 	}
 }

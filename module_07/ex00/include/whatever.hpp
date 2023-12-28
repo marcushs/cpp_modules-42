fc3195b35c471b81b21c668953381abc0f0249f3
Marcus Leung <marcus.hoksum.leung@gmail.com>
2023-12-28 17:51:34 +0100

    module07 ex00 done

---
+++ module_07/ex00/include/whatever.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hleung <hleung@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 16:47:57 by hleung            #+#    #+#             */
+/*   Updated: 2023/12/28 17:08:46 by hleung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template< typename T >
+void	swap(T &x, T &y)
+{
+	T	tmp;
+
+	tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template< typename T >
+const T	&max(const T &x, const T &y)
+{
+	return (x >= y ? x : y);
+}
+
+template< typename T >
+const T	&min(const T &x, const T &y)
+{
+	return (x <= y ? x : y);
+}
+#endif

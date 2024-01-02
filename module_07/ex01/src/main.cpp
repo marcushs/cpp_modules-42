4c2f67af7e7c0d2721fb07c5571b924ed995a262
Marcus Leung <marcus.hoksum.leung@gmail.com>
2024-01-02 15:21:47 +0100

    module07 ex01 done

--- module_07/ex01/src/main.cpp
+++ module_07/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hleung <hleung@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:46:05 by hleung            #+#    #+#             */
-/*   Updated: 2023/12/28 22:02:22 by hleung           ###   ########.fr       */
+/*   Updated: 2024/01/02 15:21:17 by hleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ int main( void )
 	std::cout << std::endl;
 
 	int	arr1[6] = {3, 5, 2, 100, 45, 99};
-	::iter<int, size_t, void>(arr1, 6, print);
+	::iter<int, size_t, void>(arr1, sizeof(arr1) / sizeof(int), print);
 
 	std::cout << std::endl;
 
+	float	arr2[10] = {86.05, 104.40, 104.17, 65.83, 93.66, 28.58, 2.82, 25.95, 91.55, 70.93};
+	::iter<float, size_t, void>(arr2, sizeof(arr2) / sizeof(float), print);
 	
 	return 0;
 }

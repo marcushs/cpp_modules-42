f89465f783fb4319282cacf916d45d65fbf89599
Hok sum Leung <hleung@z2r3p1.42lyon.fr>
2024-04-08 13:01:46 +0200

    correct ex02

--- module_08/ex02/include/MutantStack.hpp
+++ module_08/ex02/include/MutantStack.hpp
@@ -21,7 +21,7 @@ class MutantStack : public std::stack<T>
 
 public:
 	MutantStack() {}
-	MutantStack(const MutantStack &src) {*this = src;}
+	MutantStack(const MutantStack &src) : std::stack<int>(src) {*this = src;}
 	~MutantStack() {}
 
 	MutantStack	&operator=(const MutantStack &rhs)
@@ -34,6 +34,10 @@ public:
 	typedef typename std::stack<T>::container_type::iterator iterator;
 	iterator	begin() {return this->c.begin();};
 	iterator	end() {return this->c.end();};
+
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	const_iterator	begin() const {return this->c.begin();};
+	const_iterator	end() const {return this->c.end();};
 };
 
 #endif
--- module_08/ex02/src/main.cpp
+++ module_08/ex02/src/main.cpp
@@ -14,23 +14,58 @@
 #include <iostream>
 #include <list>
 
+void	iterConst(const MutantStack<int> &stack);
+
 int	main(void)
 {
+	MutantStack<int> mstack;
+	mstack.push(5); // [5]
+	mstack.push(17); // [5, 17]
+	std::cout << "Top of mstack : ";
+	std::cout << mstack.top() << std::endl; // 17
+	mstack.pop(); // [5]
+	std::cout << "Size of mstack is : ";
+	std::cout << mstack.size() << std::endl; // 1
+	mstack.push(3); //[5, 3]
+	mstack.push(5); // [5, 3, 5]
+	mstack.push(737); // [5, 3, 5, 737];
+	mstack.push(0); // [5, 3, 5, 737, 0];
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
 	{
-		MutantStack<int> mstack;
-		mstack.push(5); // [5]
-		mstack.push(17); // [5, 17]
-		std::cout << "Top of mstack : ";
-		std::cout << mstack.top() << std::endl; // 17
-		mstack.pop(); // [5]
-		std::cout << "Size of mstack is : ";
-		std::cout << mstack.size() << std::endl; // 1
-		mstack.push(3); //[5, 3]
-		mstack.push(5); // [5, 3, 5]
-		mstack.push(737); // [5, 3, 5, 737];
-		mstack.push(0); // [5, 3, 5, 737, 0];
-		MutantStack<int>::iterator it = mstack.begin();
-		MutantStack<int>::iterator ite = mstack.end();
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	size_t size = s.size();
+	for (size_t i = 0; i < size; i++)
+	{
+		std::cout << "Top of s is : "; 
+		std::cout << s.top() << std::endl; 
+		s.pop();
+	}
+	// 0 -> 737 -> 5 -> 3 -> 5
+	
+	iterConst(mstack);
+
+	{
+		std::list<int> list;
+		list.push_back(5); // [5]
+		list.push_back(17); // [5, 17]
+		std::cout << "Top of list : ";
+		std::cout << list.back() << std::endl; // 17
+		list.pop_back(); // [5]
+		std::cout << "Size of list is : ";
+		std::cout << list.size() << std::endl; // 1
+		list.push_back(3); //[5, 3]
+		list.push_back(5); // [5, 3, 5]
+		list.push_back(737); // [5, 3, 5, 737];
+		list.push_back(0); // [5, 3, 5, 737, 0];
+		std::list<int>::iterator it = list.begin();
+		std::list<int>::iterator ite = list.end();
 		++it;
 		--it;
 		while (it != ite)
@@ -38,40 +73,18 @@ int	main(void)
 			std::cout << *it << std::endl;
 			++it;
 		}
-		std::stack<int> s(mstack);
-		size_t size = s.size();
-		for (size_t i = 0; i < size; i++)
-		{
-			std::cout << "Top of s is : "; 
-			std::cout << s.top() << std::endl; 
-			s.pop();
-		}
-		// 0 -> 737 -> 5 -> 3 -> 5
 	}
-
-	// {
-	// 	std::list<int> list;
-	// 	list.push_back(5); // [5]
-	// 	list.push_back(17); // [5, 17]
-	// 	std::cout << "Top of list : ";
-	// 	std::cout << list.back() << std::endl; // 17
-	// 	list.pop_back(); // [5]
-	// 	std::cout << "Size of list is : ";
-	// 	std::cout << list.size() << std::endl; // 1
-	// 	list.push_back(3); //[5, 3]
-	// 	list.push_back(5); // [5, 3, 5]
-	// 	list.push_back(737); // [5, 3, 5, 737];
-	// 	list.push_back(0); // [5, 3, 5, 737, 0];
-	// 	std::list<int>::iterator it = list.begin();
-	// 	std::list<int>::iterator ite = list.end();
-	// 	++it;
-	// 	--it;
-	// 	while (it != ite)
-	// 	{
-	// 		std::cout << *it << std::endl;
-	// 		++it;
-	// 	}
-	// }
-	
 	return 0;
+}
+
+void	iterConst(const MutantStack<int> &stack)
+{
+	std::cout << "Iterate through a constant MutantStack : " << std::endl;
+	MutantStack<int>::const_iterator begin = stack.begin();
+	MutantStack<int>::const_iterator end = stack.end();
+	while (begin != end)
+	{
+		std::cout << *begin << std::endl;
+			++begin;
+	}
 }
